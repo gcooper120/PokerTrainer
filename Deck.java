332f297684584c568d867a103c51510800f2dcd2
sweetcoopa <cooper.george120@gmail.com>
2017-12-19 20:08:08 -0500

    Added shuffle, swapCards and printDeck methods

--- Deck.java
+++ Deck.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 /**
 	* Deck is a class used to represent ta deck of cards
 	* @author Machi
@@ -22,20 +21,98 @@ public class Deck
 	*/
 	public Deck()
 	{
+		fullDeck = new Card[52];
 		currentDeck = new Stack<Card>();
+		//Populates the ordered deck, assigning one suit at a time.
+		//i%13+1=value between 1-13,
+		//i/13+1 = value between 1-4.
+		for (int i = 51; i >= 0; i--)
+		{
+			fullDeck[i] = new Card(i%13+1, i/13+1);
+			currentDeck.push(fullDeck[i]);
+		}
+
 		ready = 0;
 	}
 
 
 	/**
 	*Shuffles a deck object and returns a stack representing the deck.
-	*@return Stack<Card> Returns the shuffled deck as a stack.
+	*@return Nothing.
 	*/
-	private Stack<Card> shuffle()
+	private void shuffle()
 	{
-
+		int randomVal;
+		int numLeft = fullDeck.length;
+		for (int i = 0; i < 52; i++, numLeft--)
+		{
+			randomVal = (int)Math.floor(Math.random() * (52-i));
+			currentDeck.push(fullDeck[randomVal]);
+			swapCards(randomVal, numLeft-1);
+		}
 		ready = 1;
-		return currentDeck;
 	}
 
+	/**
+	*Swaps two cards in the deck.
+	*@param int Index of the first card
+	*@parma int Index of the second card
+	*@return Nothing.
+	*/
+	private void swapCards(int first, int second)
+	{
+		Card temp = fullDeck[second];
+		fullDeck[second] = fullDeck[first];
+		fullDeck[first] = temp;
+	}
+
+	/**
+	*Prints the contents of a deck
+	*@return Nothing.
+	*/
+	private void printDeck()
+	{
+		for (int i = 0; i < 52; i++)
+		{
+			Card current = currentDeck.pop();
+			current.printCard();
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("TESTING");
+
+		Deck myDeck = new Deck();
+		System.out.println("==================================");
+		System.out.println("==================================");
+		System.out.println("==================================");
+		System.out.println("UNSHUFFLED");
+		myDeck.printDeck();
+		System.out.println("==================================");
+		System.out.println("==================================");
+		System.out.println("==================================");
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		myDeck.shuffle();
+		System.out.println("SHUFFLED");
+		myDeck.printDeck();
+	}
 }
