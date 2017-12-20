424acca9a2599333eb60955dfff355f342e76c47
sweetcoopa <cooper.george120@gmail.com>
2017-12-19 20:06:13 -0500

    Added printCard method and check for validity in constructor.

--- Card.java
+++ Card.java
@@ -17,7 +17,59 @@ public class Card
 	*/
 	public Card(int n, int s)
 	{
-		number = n;
-		suit = s;
+		if (s < 1 || s > 4 || n < 1 || n > 13)
+		{
+			throw new IllegalArgumentException();
+		}
+		else
+		{
+			number = n;
+			suit = s;
+		}
+	}
+
+	protected void printCard()
+	{
+		char suitChar;
+		switch (suit)
+		{
+			case 1:
+			{
+				suitChar = 'C';
+				break;
+			}
+
+			case 2:
+			{
+				suitChar = 'D';
+				break;
+			}
+			
+			case 3:
+			{
+				suitChar = 'H';
+				break;
+			}
+			
+			case 4:
+			{
+				suitChar = 'S';
+				break;
+			}	
+			default:
+			{
+				suitChar = '?';
+				break;
+			}
+		}
+
+		if (number == 1)
+		{
+			System.out.printf("%2c  %3c\n", 'A', suitChar);
+		}
+		else
+		{
+			System.out.printf("%2d  %3c\n", number, suitChar);
+		}
 	}
 }
